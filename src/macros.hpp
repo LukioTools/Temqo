72fda7080387fd61baecfcd5fc24041ddadf1a1e
PikkuHurja <pikku.hurja@gmail.com>
2023-10-12 23:44:49 +0300

    quick commit -_-

--- src/macros.hpp
+++ src/macros.hpp
@@ -5,9 +5,9 @@
 
 
 //user can define the default error type if he choses so
-#if !defined(check_default_error_type)
-#define check_default_error_type std::runtime_error
-#endif // check_default_error_type
+#if !defined(check_err_type)
+#define check_err_type std::runtime_error
+#endif // check_err_type
 
 //simple error logging
 #define err(variable) \
@@ -21,12 +21,12 @@ if((boolean) != invert_bool){\
 };
 //positive macros
 #define check(boolean, errormsg) \
-    __check(boolean, errormsg, check_default_error_type, false);
+    __check(boolean, errormsg, check_err_type, false);
 #define check_err(boolean, errormsg, errtype) \
     __check(boolean, errormsg, errtype, false);
 //inverted macros
 #define check_not(boolean, errormsg) \
-    __check(boolean, errormsg, check_default_error_type, true);
+    __check(boolean, errormsg, check_err_type, true);
 #define check_err_not(boolean, errormsg, errtype) \
     __check(boolean, errormsg, errtype, true);
 
