d075e2a82697ad6546f61d38b9f1607ba430ef8c
PikkuHurja <pikku.hurja@gmail.com>
2023-10-12 23:42:40 +0300

    quick commit -_-

--- src/macros.hpp
+++ src/macros.hpp
@@ -1,4 +1,42 @@
+#if !defined(USEFULL_MACROS)
+#define USEFULL_MACROS
+
 #include <iostream>
 
-#define log(variable) std::cout << variable << std::endl;
-#define logvar(variable) std::cout << #variable": " << variable << std::endl;
+
+//user can define the default error type if he choses so
+#if !defined(check_default_error_type)
+#define check_default_error_type std::runtime_error
+#endif // check_default_error_type
+
+//simple error logging
+#define err(variable) \
+    std::cerr << variable << std::endl;
+
+//check roots
+#define __check(boolean, errormsg, errtype, invert_bool) \
+if((boolean) != invert_bool){\
+    err(#boolean); \
+    throw errtype(errormsg);\
+};
+//positive macros
+#define check(boolean, errormsg) \
+    __check(boolean, errormsg, check_default_error_type, false);
+#define check_err(boolean, errormsg, errtype) \
+    __check(boolean, errormsg, errtype, false);
+//inverted macros
+#define check_not(boolean, errormsg) \
+    __check(boolean, errormsg, check_default_error_type, true);
+#define check_err_not(boolean, errormsg, errtype) \
+    __check(boolean, errormsg, errtype, true);
+
+//simple logging
+#define log(variable) \
+    std::cout << variable << std::endl;
+//simple variable name and value logging
+#define logvar(variable) \
+    std::cout << #variable": " << variable << std::endl;
+
+
+
+#endif // USEFULL_MACROS
--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,43 @@
 #include <iostream>
 #include <glm/ext.hpp>
 
+#include <cassert>
+
+
+
 #include <curses.h>
 #include <ncurses.h>
 
+
+//please add more usefull macros like time and replacement text 
 #include "macros.hpp"
 
+
+WINDOW* root_ptr = nullptr; // window pointer for -> root window (usually not null)
+WINDOW* ui_ptr = nullptr; // window pointer for -> user interface (usually not null)
+WINDOW* list_ptr = nullptr; // window pointer for -> song list pointer (usually not null)
+WINDOW* image_ptr = nullptr; // window pointer for -> current allocated space for the image (usually null)
+WINDOW* vis_ptr = nullptr; // window pointer for -> current allocated space for audio visualition (usually null)
+
+
+//initialize the screen and the pointers
 int startup(){
     initscr();
     return 0;
 }
 
+//dealloc all the stuff
 int stop(){
     endwin();
     return 0;
 }
 
+#define rte ;
 
 int main(int argc, char const *argv[])
 {
-    startup();
+    
+    check_err_not(1 != 1, "hello", std::logic_error);
 
     log("Hello World\n");
     
