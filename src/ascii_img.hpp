0c299d3f27d25a7c286b4c6256c144cdc41adf31
PikkuHurja <pikku.hurja@gmail.com>
2023-10-15 21:19:59 +0300

    quick commit -_-

---
+++ src/ascii_img.hpp
@@ -0,0 +1,93 @@
+
+#include <stdexcept>
+#include <sys/types.h>
+#if !defined(ASCII_IMG)
+#define ASCII_IMG 1
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#include "macros.hpp"
+
+namespace ascii_img
+{
+    struct RGB{
+        u_char r = 0;
+        u_char g = 0;
+        u_char b = 0;
+    };
+
+    inline RGB* load_image(const char* filename, int x, int y ){
+        auto out = new RGB[x*y];
+        int ix = 0, iy = 0, channels = 0;
+        stbi_uc* img = stbi_load(filename, &ix, &iy, &channels, 0);
+        logvar(filename);
+        logvar(ix);
+        logvar(iy);
+        logvar(channels);
+
+        if(!img){
+            auto err =  stbi_failure_reason();
+            logvar(err);
+            throw std::runtime_error(err);
+        }
+
+        if(!ix || !iy){
+            auto err = "Loaded image was interpeted incorrectly (x, y were zero)";
+            logvar(err);
+            throw std::runtime_error(err);
+        } 
+        auto x_step = (int) std::floor((double) ix/x);
+        auto y_step = (int) std::floor((double) iy/y);
+
+        
+
+
+        size_t iteration =0;
+        for (size_t j = 0; j < iy; j= j + (y_step*channels))
+        {
+
+            for (size_t i = 0; i < ix; i = i + (x_step*channels))
+            {
+                logvar(i+j*iy);
+                auto ptr= &img[i+j*iy];
+                //x is width
+                //y is height
+                switch (channels)
+                {
+                case 1:
+                    out[iteration] = {ptr[0],ptr[0],ptr[0]};
+                    break;
+                case 2:
+                    out[iteration] = {ptr[0], ptr[1], ptr[0]};
+                    break;
+                case 3:
+                default:
+                    out[iteration] = {ptr[0], ptr[1], ptr[2]};
+                    break;
+                }
+
+                iteration++;
+                if(iteration >= x*y){
+                    goto exit;
+                }
+            }
+            
+        }
+        
+        exit:
+
+        logvar(iteration);
+
+
+        
+
+        stbi_image_free(img);
+        return out;
+    }
+} // namespace ascii_img
+
+
+
+#endif // ASCII_IMG
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,39 @@
-///ai shit 
 
-
-
-
-#include <cstdint>
+#include <cstddef>
 #include <cstdio>
-#include <glm/fwd.hpp>
+#include <curses.h>
 #include <initializer_list>
 #include <iostream>
-#include <glm/ext.hpp>
-#include <thread>
-#include <locale.h>
-
-#include <glm/ext.hpp>
-
-#include <cassert>
-
-// -lncurses
-#include <curses.h>
-#include <ncurses.h>
+#include <iterator>
+#include <new>
+#include <ostream>
+#include <stdexcept>
+#include <sys/types.h>
 #include <vector>
+#include <ncurses.h>
+#include <glm/ext.hpp>
+#include <fstream>
+
+std::ofstream different_terminal("/dev/pts/3");
+
+WINDOW* log_scr = nullptr;
+
+#define LOG_STREAM different_terminal
+#include "macros.hpp"
+#define LOG_WIN log_scr
+#include "nc_macros.hpp"
+
+#include "ascii_img.hpp"
 
 class Window
 {
 private:
 public:
     WINDOW* window = nullptr;
-    Window() : window(stdscr) {}
-    Window(uint_fast16_t width, uint_fast16_t height, uint_fast16_t x_offset, uint_fast16_t y_offset) : window(stdscr) {
+    Window(const  Window &)= delete;
+    Window() : window(nullptr) {}
+    Window(WINDOW* win) : window(win) {}
+    Window(uint_fast16_t width, uint_fast16_t height, uint_fast16_t x_offset, uint_fast16_t y_offset) : window(nullptr) {
         window = subwin(stdscr, width, height, x_offset, y_offset);
     }
 
@@ -47,6 +53,10 @@ public:
         return window;
     }
 
+
+
+    Window& operator=(const  Window&)  = delete;
+
     Window& operator=(WINDOW* wind){
         if(exists()){
             delwin(window);
@@ -64,147 +74,288 @@ public:
 };
 
 
+struct Element
+{
+    Window window;
+    size_t size = 0;
+};
+
+
 class Row
 {
 private:
+    std::vector<Element*> vec;
     
 public:
-    Window* windows; //null terminated?
-    ushort relative_height;
-    Row() {
+
+    Row(){};
+    Row(std::initializer_list<Element*> el):vec(el){};
+    ~Row() = default;
+
+
+    Element* append(Element* el){
+        vec.push_back(el);
+        return el;
     }
-    ~Row() {}
+    Element* append(size_t relative_size){
+        auto el = new Element{Window(nullptr), relative_size};
+        vec.push_back(el);
+        return el;
+    }
+    void insert(Element* el, size_t position){
+        auto it = vec.begin();
+        std::advance(it, position);
+        if(it >= vec.end()) throw std::out_of_range("Insert was out of range");
+        vec.insert(it, el);
+    }
+    void insert(Element** el, size_t n, size_t position){
+        auto it = vec.begin();
+        std::advance(it, position);
+        if(it >= vec.end()) throw std::out_of_range("Insert was out of range");
+        vec.insert(it, el, el+n);
+    }
+
+    void resize(size_t idx, long pm){
+        vec[idx]->size+=pm;
+    }
+
+    Element* operator[](size_t idx){
+        return vec[idx];
+    }
+
+
+
+    int refresh(long height, size_t height_offset){
+        uint x,y;
+        getmaxyx(stdscr, y,x);
+        if(height < 0){
+            height = y+height;
+        }
+        
+        if(vec.size() == 0) return -1;
+        size_t 
+            percent = 0,
+            pos = 0;
+        for (auto &&i : vec)
+        {
+            percent += i->size;
+        }
+        
+        if(!percent) return -1;
+
+        for (auto &&i : vec)
+        {
+            size_t width = round((double)(x*(i->size))/percent);
+            if(width+pos > x){width = x-pos;}
+
+            i->window = subwin(stdscr, height, width, height_offset, pos);
+            if(height > 2)
+                box(i->window, 0,0);
+            wrefresh(i->window);
+            pos += width;
+        }
+        return OK;
+    }
 };
 
+enum RowType: u_char{
+    UNDEFINED,
+    RELATIVE,
+    PERCENT,
+    ABSOLUTE,
+};
+
+struct RowElement{
+    Row row;
+    uint size = 1;
+    RowType type = RELATIVE;
+    uint calculate_height(size_t abs_area, uint relative_percent = 1){
+        uint x,y;
+        getmaxyx(stdscr, y,x);
+        double idk;
+        switch (type) {
+            case RowType::UNDEFINED:
+            case RowType::RELATIVE:
+                idk = round((double)((y-abs_area)*size)/relative_percent);
+                logvar(idk)
+                logvar(y-abs_area)
+                logvar((double) size/relative_percent)
+                return idk;
+            case PERCENT:
+                return round((double) size/100);
+            case ABSOLUTE:
+                return size;
+            default:
+                return size;
+        }
+    }
+};
+
+//aka just a bunch of rows
 class WindowManager
 {
 private:
-    
+    std::vector<RowElement*> rows;
 public:
-    std::vector<Window*> windows;
+
+
+    void append(RowElement* re){
+        rows.push_back(re);
+    }
+
+    void refresh(){
+        uint x,y;
+        getmaxyx(stdscr, y,x);
+        size_t pos = 0, percent = 0, absolute_space = 0; 
+        for (auto &&i : rows)
+        {
+            if(i->type == RowType::RELATIVE || i->type == RowType::UNDEFINED){
+                percent+=i->size;
+            }
+            if(i->type == RowType::ABSOLUTE){
+                absolute_space += i->size;
+            }
+        }
+        for (auto &&i : rows)
+        {
+            size_t height = i->calculate_height(absolute_space, percent);
+            logvar(height);
+            if(height+pos > y){height = y-pos;}
+            i->row.refresh(height, pos);
+            pos+=height;
+        }
+        
+    }
+
+    void resize(size_t idx, long pm){
+        rows[idx]->size+=pm;
+    }
+
+    RowElement* operator[](size_t idx){
+        return rows[idx];
+    }
+
+    size_t size(){
+        return rows.size();
+    }
     
     WindowManager() {}
-    WindowManager(std::initializer_list<Window*> ls): windows(ls) {}
-    
+    WindowManager(std::initializer_list<RowElement*> ls): rows(ls) {}
     ~WindowManager() {}
 };
-
-Window root_ptr;        // window pointer for -> root window (usually not null)
-Window ui_ptr;          // window pointer for -> user interface (usually not null)
-Window log_ptr;         // window pointer for -> user interface (usually not null)
-Window list_ptr;        // window pointer for -> song list pointer (usually not null)
-Window playlists_ptr;   // window pointer for -> playlist pointer (usually not null)
-Window image_ptr;       // window pointer for -> current allocated space for the image (usually null)
-Window vis_ptr;         // window pointer for -> current allocated space for audio visualition (usually null)
-
-
-//please add more usefull macros like time and replacement text 
-#include "macros.hpp"
-#define LOG_WIN log_ptr
-#include "nc_macros.hpp"
-
-
-
-
-
-
-class Border{
-public:
-    wchar_t 
-        left, 
-        right, 
-        top, 
-        bottom, 
-        top_left, 
-        top_right, 
-        bottom_left, 
-        bottom_right
-
-    ;
-
-    Border(chtype _left = 0,chtype _right = 0,chtype _top = 0,chtype _bottom = 0,chtype _top_left = 0,chtype _top_right = 0,chtype _bottom_left = 0,chtype _bottom_right = 0): 
-    left(_left), right(_right), top(_top), bottom(_bottom), top_left(_top_left), top_right(_top_right), bottom_left(_bottom_left), bottom_right(_bottom_right) 
-    {
-    }
-
-    void draw(Window& win){
-        //if(!win.exists()) {
-        //    std::cerr << "Window didnt exist\n" << std::endl;
-        //    return;
-        //};
-//
-        //glm::ivec2 size = win.size();
-
-    }
-    
-};
-
-Border left_border(0,0,0,0,0, 0,0, 0);
-
-
-
-
-//initialize the screen and the pointers
 int startup(){
     initscr();
     cbreak(); // Disable line buffering
     noecho(); // Don't echo user input
     keypad(stdscr, TRUE); // Enable function keys
     refresh();
-
-    root_ptr = stdscr;      
-    ui_ptr = stdscr;        
-    log_ptr = stdscr;       
-    list_ptr = stdscr;      
-    playlists_ptr = stdscr; 
-    image_ptr = stdscr;     
-    vis_ptr = stdscr;       
-    
-
-    get_max(width, height);
-
-    constexpr int text_size = 1;
-
-    log_ptr = subwin(stdscr, text_size, width, 0, 0);
-    ui_ptr = subwin(stdscr, text_size, width, height - text_size, 0);
-    playlists_ptr = subwin(stdscr, height- 2*text_size, width * 1/9, 1, 0);
-    list_ptr = subwin(stdscr, height- 2*text_size, width * 1/9, 1, 0);
-    
-
-    left_border.draw(playlists_ptr);
-    //nc_log("Exists: %s (%p) [%p] {%p}\n", playlists_ptr.exists() ? "True":"False", playlists_ptr.window, ptr,stdscr);
-
-    wrefresh(playlists_ptr);
-
-
+    log_scr = stdscr;
     return 0;
 }
 
-//dealloc all the stuff
 int stop(){
     endwin();
     return 0;
 }
 
 
+enum InputMode: u_char{
+    COMMAND,
+    TEXT,
+};
+InputMode mode = InputMode::COMMAND;
+
+
 int main(int argc, char const *argv[])
 {
+    logvar("\nStarting program\n");
+    startup();
+    WindowManager wm;
+    wm.append(new RowElement{Row(), 1, RowType::ABSOLUTE});
+    wm.append(new RowElement{Row(), 2, RowType::RELATIVE});
+    wm.append(new RowElement{Row(), 1, RowType::ABSOLUTE});
+
+    auto log_element = wm[0]->row.append(2);
+
+
     
-
-    check(startup(), "Library initialization failed");
-    
-    get_max(width, height);
-    
-    
-    //nc_log("Hello World (width:%i, height:%i)", width, height);
-
-    wprintw(ui_ptr.window, "Command: ");
-    wrefresh(ui_ptr.window);
-
-    getch();
-
-    check(stop(), "Library de-initialization failed");
+    //wm[0]->row.append(7);
 
 
+    wm[1]->row.append(6);
+    wm[1]->row.append(3);
+    wm[1]->row.append(2);
+
+    Element* text_element = wm[2]->row.append(1);
+
+    wm.refresh();
+
+
+    std::string input;
+
+
+
+
+
+    while(auto c = wgetch(stdscr)){
+        wclear(stdscr);
+        wm.refresh();
+        refresh();
+        log_scr = log_element->window;
+
+        switch (mode) {
+            case InputMode::COMMAND:
+            {
+                if(c == 'q') { //resize event
+                    goto exit;
+                }
+                else if(c=='i'){
+                    mode = InputMode::TEXT;
+                    input = "";
+                    disp(text_element->window, ":%s", input.c_str());
+                    break;
+                }
+                else{
+                    disp(log_scr, "(%i): %c", c, c);
+                }
+            }
+            break;
+            case InputMode::TEXT: 
+            {
+                switch (c)
+                {
+                case 10:
+                    //enter
+                    disp(log_scr, "input: %s", input.c_str());
+
+                    //do something with the input
+
+                    input = "";
+                    break;
+
+                case 263:
+                    if(input.length() > 0)
+                        input.pop_back();
+                case 410:
+                    disp(text_element->window, ":%s", input.c_str());
+                    break;
+
+
+                case 27:
+                    mode = InputMode::COMMAND;
+                    break;
+
+                default:
+                    input+=c; //263 //27???
+                    disp(text_element->window, ":%s", input.c_str());
+                }
+            }
+        }
+    }
+
+#undef disp_input
+
+    exit:
+    stop();
     return 0;
 }
